8a46161e69c036c9432f4bcac88c837132244dfd
李嘉博 <1658446745@qq.com>
2022-03-28 15:24:42 +0800

    创建IDEA工程

---
+++ src/src/main/java/top/jiyuanapp/jiyuan/HelloApplication.java
@@ -0,0 +1,9 @@
+package top.jiyuanapp.jiyuan;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
